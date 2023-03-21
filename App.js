a1d9d04d2901aee81b4ac999ded77487835491be
Md Amir Hossain <amirhossain.limon@gmail.com>
2023-03-21 15:23:23 +0600

    Push my first commit in the repo.

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo, FontAwesome } from '@expo/vector-icons';
+
+import SearchScreen from './screens/SearchScreen';
+import HomeStack from './Layout/HomeStack';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <Tab.Navigator
+          screenOptions={{ tabBarActiveTintColor: 'red' }}
+          initialRouteName='Home'
+        >
+          <Tab.Screen
+            name='Home'
+            component={HomeStack}
+            options={{
+              tabBarIcon: () => <Entypo name='home' size={24} color='#000' />,
+              headerTitle: 'বাংলা গানের কথা, লিরিক্স বাংলা | Lyrics Bangla',
+              tabBarLabel: 'Home',
+            }}
+          />
+          <Tab.Screen
+            name='Search'
+            component={SearchScreen}
+            options={{
+              tabBarIcon: () => (
+                <FontAwesome name='search' size={24} color='#000' />
+              ),
+              headerTitle: 'বাংলা গানের কথা/লিরিক্স খুজুন | Lyrics Bangla',
+              tabBarLabel: 'Search',
+            }}
+          />
+        </Tab.Navigator>
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
