b005237dbd07c5dcf4858de6998f8fb7c2a7a59b
Md Amir Hossain <amirhossain.limon@gmail.com>
2023-04-08 22:15:10 +0600

    added toggle buttne

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,10 +1,55 @@
 import React from "react";
-import { Box, Heading } from "native-base";
+import {
+  Box,
+  Divider,
+  Heading,
+  HStack,
+  ScrollView,
+  Switch,
+  Text,
+  useColorMode,
+} from "native-base";
+import { Ionicons } from "@expo/vector-icons";
+import ToggleMode from "../../components/ToggleMode";
 
-export const Home = () => {
+export const Home = ({ navigation }) => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Box>
-      <Heading>Lyrics Bangla</Heading>
+    <Box bg={colorMode === "dark" ? "black" : "white"} pt={12}>
+      <ScrollView contentContainerStyle={{ width: "100%" }}>
+        <Heading p={3} mx={2}>
+          Lyrics Bangla
+        </Heading>
+        <Divider opacity={colorMode === "dark" ? "0.4" : "1"} />
+        <HStack alignItems="center" space={6} py={4} px={3} mx={2}>
+          <Ionicons
+            name="sunny-sharp"
+            size={24}
+            color={colorMode === "dark" ? "white" : "black"}
+          />
+          <Text>Light Mode</Text>
+
+          <Switch
+            mx="auto"
+            onToggle={toggleColorMode}
+            isChecked={colorMode === "dark"}
+            aria-label={
+              colorMode === "dark"
+                ? "switch to light mode"
+                : "switch to dark mode"
+            }
+          />
+          <Text>Dark Mode</Text>
+          <Ionicons
+            name="moon-sharp"
+            size={24}
+            color={colorMode === "dark" ? "white" : "black"}
+          />
+        </HStack>
+        <Divider opacity={colorMode === "dark" ? "0.4" : "1"} />
+        <Divider mt={12} opacity={colorMode === "dark" ? "0.4" : "1"} />
+      </ScrollView>
     </Box>
   );
 };
